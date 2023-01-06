527509c6a9561f834b6c71d1583da7ea6ebaf909
이상학 <65946607+yee950419@users.noreply.github.com>
2023-01-06 20:12:15 +0900

    [Silver III] Title: N과 M (2), Time: 232 ms, Memory: 18324 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int[] arr = new int[M];
+		Arrays.fill(arr, 0);
+		boolean[] visit = new boolean[N];
+		
+		dfs(N, M, 0, arr, visit);
+	}
+ 
+	public static void dfs(int N, int M, int index, int[] arr, boolean[] visit) {
+
+		if (index == M) {
+			for(int i=0; i<arr.length; i++) {
+				System.out.print(arr[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			if (!visit[i]) {
+				visit[i] = true;
+				arr[index] = i + 1;
+				if(index == 0) {
+					dfs(N, M, index + 1, arr, visit);
+				}
+				else {
+					if(arr[index-1] < arr[index]) {
+						dfs(N, M, index + 1, arr, visit);
+					}
+				}
+				visit[i] = false;
+			}
+		}
+	}
+}
