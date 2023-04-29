27ffc702ded35536cf1d767881bdd70d1f6a2767
이상학 <65946607+yee950419@users.noreply.github.com>
2023-04-29 14:59:17 +0900

    [D2] Title: Base64 Decoder, Time: 101 ms, Memory: 18,488 KB -BaekjoonHub

---
+++ SWEA/D2/1928. Base64 Decoder/Base64 Decoder.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Base64;
+import java.util.StringTokenizer;
+
+class Solution {
+	
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static StringTokenizer st;
+	private static StringBuilder sb = new StringBuilder();
+	
+	private static int T;
+	private static int N;
+	
+	public static void main(String[] args) throws IOException {
+		
+		st = new StringTokenizer(br.readLine());
+		T = Integer.parseInt(st.nextToken());
+		
+		loopTest();
+		System.out.println(sb);
+	}
+	
+	static void loopTest() throws IOException {
+		
+		for(int testCase=1; testCase<=T; testCase++) {
+			
+			sb.append("#" + testCase + " ");
+			
+			getDecodedString();
+		}
+	}
+	
+	static void getDecodedString() throws IOException {
+		
+		String incodedString = "";
+		String decodedString = "";
+		
+		st = new StringTokenizer(br.readLine());
+		incodedString = st.nextToken();
+		decodedString = new String(Base64.getDecoder().decode(incodedString));
+		
+		sb.append(decodedString + "\n");
+	}
+}
