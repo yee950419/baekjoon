2fba12b00f529e5c21a25b9ce07598370398ed26
이상학 <65946607+yee950419@users.noreply.github.com>
2023-05-05 14:09:22 +0900

    [D3] Title: 통나무 자르기, Time: 108 ms, Memory: 17,428 KB -BaekjoonHub

---
+++ SWEA/D3/14692. 통나무 자르기/통나무 자르기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Solution {
+	
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static StringTokenizer st;
+	private static StringBuilder sb = new StringBuilder();
+	
+	private static int T;
+	private static int N;
+	
+	public static void main(String[] args) throws IOException {
+		
+		st = new StringTokenizer(br.readLine());
+		T = Integer.parseInt(st.nextToken());
+		
+		loopTest();
+		System.out.println(sb);
+	}
+	
+	static void loopTest() throws IOException {
+		
+		for(int testCase=1; testCase<=T; testCase++) {
+			
+			sb.append("#" + testCase + " ");
+			
+			getWinner();
+		}
+	}
+	
+	static void getWinner() throws IOException {
+		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		
+		if(N % 2 == 0) {
+			sb.append("Alice" + "\n");
+		}
+		else {
+			sb.append("Bob" + "\n");
+		}
+	}
+}
