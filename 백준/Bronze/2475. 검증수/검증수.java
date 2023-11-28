e2356e736d91ed030efa99283180f72289259a58
이상학 <65946607+yee950419@users.noreply.github.com>
2023-11-28 09:28:49 +0900

    [Bronze V] Title: 검증수, Time: 128 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<5; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            sum += num*num;
+        }
+
+        System.out.println(sum % 10);
+    }
+}
