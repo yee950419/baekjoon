bbc9f6aec386ba1e6dc38cd6d16711e3cfb08c1f
이상학 <65946607+yee950419@users.noreply.github.com>
2024-06-29 16:35:34 +0900

    [Silver II] Title: 점프 점프, Time: 108 ms, Memory: 14416 KB -BaekjoonHub

---
+++ 백준/Silver/11060. 점프 점프/점프 점프.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] array = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			
+			array[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[] sumArray = new int[N];
+		for(int i=1; i<N; i++) {
+			sumArray[i] = 1001;
+		}
+		sumArray[0] = 0;
+		for(int i=0; i<N; i++) {
+			
+			for(int j=i+1; j<=array[i]+i; j++) {
+				
+				if(j == N) {
+					break;
+				}
+				if(sumArray[j] > sumArray[i]) {
+					sumArray[j] = sumArray[i] + 1;					
+				}
+			}
+		}
+		
+		if(sumArray[N-1] == 1001) {
+			sumArray[N-1] = -1;
+		}
+		
+		System.out.println(sumArray[N-1]);
+	}
+}
