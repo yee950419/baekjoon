214d0066a15f2907abee02d1fcea74e3fc7bfd3f
이상학 <65946607+yee950419@users.noreply.github.com>
2024-07-11 17:32:47 +0900

    [Silver I] Title: 트럭, Time: 124 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		
+		Queue<Integer> truckQ = new ArrayDeque<>();
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			
+			truckQ.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		Queue<Integer> onBridgeQ = new ArrayDeque<>();
+		for(int i=0; i<w; i++) {
+			onBridgeQ.add(0);
+		}
+		
+		int time = 0;
+		int sumWeight = 0;
+		while(!truckQ.isEmpty()) {
+			
+			sumWeight -= onBridgeQ.poll();
+			if(sumWeight + truckQ.peek() <= L) {
+				
+				int truck = truckQ.poll();
+				onBridgeQ.add(truck);
+				sumWeight += truck;
+			}
+			else {
+				
+				onBridgeQ.add(0);
+			}
+			
+			time++;
+		}
+			
+		System.out.println(time + w);
+	}
+}
